1175bf26b8cb54b4c74d2a6e93aa052e7e1ab26f
yuki7725 <147121669+yuki7725@users.noreply.github.com>
2024-05-19 13:55:14 +0900

    2-1 p18

---
+++ DirectXGame/player.cpp
@@ -0,0 +1,7 @@
+#include "player.h"
+
+void Player::Initialize() {}
+
+void Player::Update() {}
+
+void Player::Draw() {}
--- DirectXGame/player.h
+++ DirectXGame/player.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <Model.h>
+#include <WorldTransform.h>
 
 class Player {
+public:
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+private:
+
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+
+	//モデル
+	Model* model_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
 
 };

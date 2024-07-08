3e4e06b169e95c8d36ca4ac1269d77a47873bfad
yuki7725 <147121669+yuki7725@users.noreply.github.com>
2024-07-08 13:01:57 +0900

    2

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -78,11 +78,12 @@ void GameScene::Initialize() {
 	//キューブの生成
 	for (uint32_t i = 0; i < kNumBlockHorizontal; ++i) {
 		for (uint32_t j = 0; j < kNumBlockVertical; ++j) {
-
-			worldTransformBlocks_[j][i] = new WorldTransform();
-			worldTransformBlocks_[j][i]->Initialize();
-			worldTransformBlocks_[j][i]->translation_.x = kBlockWidth * j;
-			worldTransformBlocks_[j][i]->translation_.y = kBlockHeight * i;
+			if (map[j][i] == 1) {
+				worldTransformBlocks_[j][i] = new WorldTransform();
+				worldTransformBlocks_[j][i]->Initialize();
+				worldTransformBlocks_[j][i]->translation_.x = kBlockWidth * i;
+				worldTransformBlocks_[j][i]->translation_.y = kBlockHeight * j;
+			}
 		}
 	}
 

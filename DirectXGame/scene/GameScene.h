aa31fabd6daffd2e0b4dbbabd91cd67b5e83749b
yuki7725 <147121669+yuki7725@users.noreply.github.com>
2024-07-25 12:01:00 +0900

    1

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -57,12 +57,9 @@ private: // メンバ変数
 	// 3Dモデルデータ
 	Model* model_ = nullptr;
 	Model* modelSkyDome_ = nullptr;//天球
-<<<<<<< Updated upstream
 
-=======
 	Model* modelPlayer_ = nullptr;
 	
->>>>>>> Stashed changes
 	// ワールドトランスフォーム
 	WorldTransform worldTransform_;
 
@@ -74,11 +71,8 @@ private: // メンバ変数
 	// 自キャラ
 	Player* player_ = nullptr;
 
-<<<<<<< Updated upstream
 	Model* modelBlock_ = nullptr;
 
-=======
->>>>>>> Stashed changes
 	//デバッグカメラ
 	bool isDebugCameraActive_ = false;
 

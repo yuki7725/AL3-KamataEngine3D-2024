cf94c35b72b0cffeabd89816ef72d649c49a399f
yuki7725 <147121669+yuki7725@users.noreply.github.com>
2024-07-29 15:59:51 +0900

    p17

--- DirectXGame/CameraController.h
+++ DirectXGame/CameraController.h
@@ -34,6 +34,12 @@ private:
 	//追従対象とカメラの座標の差(オフセット)
 	Vector3 targetOffset_ = {0, 0, -30.0f};
 
-
+	//矩形
+	struct Rect {
+		float left = 0.0f;//左端
+		float right = 1.0f;//右端
+		float bottom = 0.0f;//下端
+		float top = 1.0f;//上端
+	};
 
 };
